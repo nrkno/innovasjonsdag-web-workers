c3302343db08b4fd7649ab17bd13d0440717fa06
Skjalg Teig <skjalgepalg@gmail.com>
2023-12-08 12:05:15 +0100

    feat: generate all frames as bitmaps, then render the loop

--- offscreen.js
+++ offscreen.js
@@ -1,55 +1,25 @@
-import { TWO_PI, getNoiseOffset, getRadialOffset } from './utils'
+import { drawFrame } from './utils'
 
 onmessage = evt => {
   const { cx, cy, x0, y0, W, H, wW, hH, canvas } = evt.data
-  const getCenteredRadOffset = (x, y) => getRadialOffset(x, y, cx, cy)
 
   const /** @type CanvasRenderingContext2D */ ctx = canvas.getContext('2d')
 
   // 25 frames per second for 5 seconds => 125 frames per loop
   const TOTALT_FRAME_N = 25 * 5
+  const allFrames = new Array(TOTALT_FRAME_N).fill(0).map((_, i) => i)
 
-  const drawFrame = n => {
-    let progress = n / TOTALT_FRAME_N
-    ctx.fillStyle = '#000'
-
-    // console.log({progress})
-    ctx.fillRect(0, 0, wW, hH)
-    ctx.save()
-
-    ctx.globalCompositeOperation = 'lighter'
-    // console.log({ x0: x0, W: W })
-    // Loop over all pixels inside margin on the x-axis
-    for (let i = x0; i < W; i += 5) {
-      // Loop over all pixels inside margin on the y-axis
-      for (let j = y0; j < H; j += 20) {
-        let x = i
-        let y = j
-        const distancefactor = getCenteredRadOffset(x, y) / getCenteredRadOffset(0, 0)
-        const vertdistancefactor = getCenteredRadOffset(0, y) / getCenteredRadOffset(0, 0)
-
-        const g = 75 * 2
-        const r = 1 * (1 + 65 * vertdistancefactor)
-        const b = 128 * 2
-        ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
-        const taper = 50 * (1 - 0.9 * distancefactor)
-
-        let dx = taper * getNoiseOffset(progress - 0.001 * getCenteredRadOffset(x, y), 0, x, y)
-        let dy = taper * getNoiseOffset(progress - 0.001 * getCenteredRadOffset(x, y), 150, x, y)
-
-        ctx.beginPath()
-        ctx.arc(x + dx, y + dy, 1, 0, TWO_PI)
-        ctx.fill()
-      }
-    }
-
-    ctx.restore()
-  }
-
+  const drawSingleFrame = (frame) => drawFrame(canvas, ctx, frame, TOTALT_FRAME_N, wW, hH, W, H, x0, y0, cx, cy)
+  const allBitmaps = allFrames.map((val) => drawSingleFrame(val))
+  // console.log({allFrames, allBitmaps})
+  // allBitmaps.forEach((bmp) => {
+  //   requestAnimationFrame(ctx.drawImage(bmp, 0, 0)
+  //   bmp.transferFromImageBitmap()
+  // })
   const loop = frameN => {
     // console.log({ frameN })
-    drawFrame(frameN)
-    if (frameN < TOTALT_FRAME_N) {
+    ctx.drawImage(allBitmaps[frameN], 0, 0)
+    if (frameN < TOTALT_FRAME_N - 1) {
       requestAnimationFrame(loop.bind(null, frameN + 1))
     } else {
       requestAnimationFrame(loop.bind(null, 0))
--- utils.js
+++ utils.js
@@ -26,3 +26,41 @@ export const getNoiseOffset = (progress, offset, x, y) => {
     S * x
   )
 }
+
+export const drawFrame = (canvas, ctx, frame, totalFrames, wW, hH, W, H, x0, y0, cx, cy) => {
+  // console.log({canvas, ctx, frame, totalFrames, wW, hH, W, H, x0, y0, cx, cy})
+  const getCenteredRadOffset = (x, y) => getRadialOffset(x, y, cx, cy)
+  let progress = frame / totalFrames
+  ctx.fillStyle = '#000'
+
+  ctx.fillRect(0, 0, wW, hH)
+  ctx.save()
+
+  ctx.globalCompositeOperation = 'lighter'
+  // Loop over all pixels inside margin on the x-axis
+  for (let i = x0; i < W; i += 3) {
+    // Loop over all pixels inside margin on the y-axis
+    for (let j = y0; j < H; j += 20) {
+      let x = i
+      let y = j
+      const distancefactor = getCenteredRadOffset(x, y) / getCenteredRadOffset(0, 0)
+      const vertdistancefactor = getCenteredRadOffset(0, y) / getCenteredRadOffset(0, 0)
+
+      const g = 75 * 2
+      const r = 1 * (1 + 65 * vertdistancefactor)
+      const b = 128 * 2
+      ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
+      const taper = 50 * (1 - 0.9 * distancefactor)
+
+      let dx = taper * getNoiseOffset(progress - 0.001 * getCenteredRadOffset(x, y), 0, x, y)
+      let dy = taper * getNoiseOffset(progress - 0.001 * getCenteredRadOffset(x, y), 150, x, y)
+
+      ctx.beginPath()
+      ctx.arc(x + dx, y + dy, 1, 0, TWO_PI)
+      ctx.fill()
+    }
+  }
+  ctx.restore()
+  const bitmap = canvas.transferToImageBitmap()
+  return bitmap
+}
